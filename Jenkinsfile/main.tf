7b6d81c37d213f366b17d003e24a23e09bb1ba52
Shreehari <shreeharibandrawad@gmail.com>
2024-09-17 12:30:32 +0530

    Jenkins file created

---
+++ Jenkinsfile/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "instance-1" {
+    ami = "ami-037774efca2da0726"
+    instance_type = "t2.micro"
+    count = "1"
+    security_groups = ["default"]
+    key_name = "test-MD"
+    tags = {
+      Name = "grafan"
+    }
+}
+
+resource "aws_instance" "instance-2" {
+    ami = "ami-037774efca2da0726"
+    instance_type = "t2.micro"
+    count = "1"
+    security_groups = ["default"]
+    key_name = "test-MD"
+    tags = {
+      Name = "Node-port"
+    }
+}
