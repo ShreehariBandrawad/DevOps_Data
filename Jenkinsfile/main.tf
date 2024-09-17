34d18af08c7c9f84b7cf4281a5b943890fd3f473
Shreehari <shreeharibandrawad@gmail.com>
2024-09-17 18:35:34 +0530

    updated the jenkins file

--- Jenkinsfile/main.tf
+++ Jenkinsfile/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "instance-1" {
 }
 
 resource "aws_instance" "instance-2" {
-    ami = "ami-037774efca2da0726"
+    ami = "ami-037774efca2da0726"    
     instance_type = "t2.micro"
     count = "1"
     security_groups = ["default"]
